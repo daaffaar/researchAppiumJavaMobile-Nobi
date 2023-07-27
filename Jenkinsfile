0200a36cfd6c44473c41431dcf8c80c676d18dbd
Daffa Ramadhan <daffa.ramadhan@usenobi.com>
2023-07-27 18:22:21 +0700

    deleted changes commit before this

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,31 +1,16 @@
 pipeline {
     agent any
-    environment {
-            APPETIZE_API_TOKEN = credentials('appetize_api_token')
-            APPETIZE_PUBLIC_KEY = 'epefc2mnhisphbdawvhtdhf5la'
-//             APK_PATH = 'epefc2mnhisphbdawvhtdhf5la'
-        }
     stages {
         stage('Build') {
             steps {
                 checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/daaffaar/researchAppiumJavaMobile-Nobi.git']]])
            }
         }
-//         stage('Upload to Appetize.io') {
-//                     steps {
-//                         bat "appetize upload --token $APPETIZE_API_TOKEN --path $APK_PATH"
-//                     }
-//                 }
-                stage('Launch Appetize.io Emulator') {
-                    steps {
-                        bat "appetize launch --token $APPETIZE_API_TOKEN --publicKey $APPETIZE_PUBLIC_KEY"
-                    }
-                }
         stage('Test') {
             steps {
                 bat 'mvn clean test -Dtest=base'
             }
         }
-
     }
 }
+
--- src/test/java/testCase/Earn/base.java
+++ src/test/java/testCase/Earn/base.java
@@ -3,7 +3,9 @@ package testCase.Earn;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -16,18 +18,11 @@ public class base {
 
     @Test
     public static void LaunchTest() throws MalformedURLException, InterruptedException {
-//        UiAutomator2Options options = new UiAutomator2Options();
-//        options.setPlatformName("Android");
-//        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-//        options.setDeviceName("Daffa_Emu");
-//        options.setApp("D://New GIT//nobi-automation-katalon-mobile-android//androidapp//Nobi_Dev_base.apk");
-//        options.setNoReset(false);
-
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-        options.setDeviceName("Android Emulator");
-        options.setApp("http://appetize.io/embed/epefc2mnhisphbdawvhtdhf5la?platform=android");
+        options.setDeviceName("Daffa_Emu");
+        options.setApp("D://New GIT//nobi-automation-katalon-mobile-android//androidapp//Nobi_Dev_base.apk");
         options.setNoReset(false);
 
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
